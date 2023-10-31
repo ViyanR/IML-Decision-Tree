1d16251e572323579574a86990ee84322dab3a23
vr121 <vr121@ic.ac.uk>
2023-10-31 15:41:27 +0000

    Completed decision_tree_learning. Has bugs.

--- decision_tree.py
+++ decision_tree.py
@@ -9,10 +9,9 @@ def load_dataset(filepath):
 
 def decision_tree_learning(dataset, depth):
     labels = dataset[:, 7]
-    print(labels)
+    print(dataset)
     
     if np.all(labels[0] == labels):
-        pass
         # Base Case
         # Return leaf node with this value, depth
         return {"class": labels[0]}
@@ -20,7 +19,7 @@ def decision_tree_learning(dataset, depth):
         # Recursive case
         # Find the optimum split
         root_node = find_split(dataset)
-        (left_data, right_data) = split_data(root_node["attribute"], root_node["value"])
+        (left_data, right_data) = split_data(dataset, root_node["attribute"], root_node["value"])
         root_node["left"], l_depth = decision_tree_learning(left_data, depth+1)
         root_node["right"], r_depth = decision_tree_learning(right_data, depth+1)
         return (root_node, max(l_depth, r_depth))
@@ -45,13 +44,43 @@ def find_split(dataset):
 def find_split_value(dataset, attribute):
         attributeValues = dataset[:, attribute]
         sortedAttributeValues = np.sort(attributeValues)
+        maxIg = 0
+        maxIgValue = None
+        
         for i in range(len(sortedAttributeValues)-1):
             split_value = (sortedAttributeValues[i] + sortedAttributeValues[i+1])/2
+            (left_data, right_data) = split_data(dataset, attribute, split_value)
+            ig = entropy(dataset) - remainder(left_data, right_data)
+            if ig > maxIg:
+                maxIg = ig
+                maxIgValue = split_value
             
-        print(labels)
-        return(5, 2)
+        return(maxIgValue, maxIg)
 
-def split_data(attribute, value):
+def entropy(dataset):
+    ans = 0
+    labels = dataset[:, 7]
+    label_counts = {}
+    for label in labels:
+        label = str(int(label))
+        label_counts[label] = label_counts.get(label, 0) + 1
+    for i in range(1, 7):
+        probability = label_counts.get(str(i), 0)/len(labels)
+        ans += probability * np.log2(probability)
+    ans *= -1
+    return ans
+
+def remainder(left_data, right_data):
+    left_size, _ = left_data.shape
+    right_size, _ = right_data.shape
+    
+    left_remainder = left_size/(left_size + right_size) * entropy(left_data)
+    right_remainder = right_size/(left_size+right_size) * entropy(right_data)
+    
+    return left_remainder + right_remainder
+    
+
+def split_data(data, attribute, value):
     left_data = data[data[:, attribute] < value]
     right_data = data[data[:, attribute] >= value]
     return (left_data, right_data)
