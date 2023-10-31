7e10f3d79a0757e5489bb421226c2a1025890926
vr121 <vr121@ic.ac.uk>
2023-10-31 14:11:10 +0000

    Started data splitting

--- decision_tree.py
+++ decision_tree.py
@@ -1,12 +1,60 @@
 import numpy as np
 import sys
 
+LABEL_COLUMN = 7
+
 def load_dataset(filepath):
     data = np.loadtxt(filepath)
     return data
 
 def decision_tree_learning(dataset: np.ndarray, depth: int):
-    print(dataset)
+    labels = dataset[:, 7]
+    print(labels)
+    
+    if np.all(labels[0] == labels):
+        pass
+        # Base Case
+        # Return leaf node with this value, depth
+        return {"class": labels[0]}
+    else:
+        # Recursive case
+        # Find the optimum split
+        root_node = find_split(dataset)
+        (left_data, right_data) = split_data(root_node["attribute"], root_node["value"])
+        root_node["left"], l_depth = decision_tree_learning(left_data, depth+1)
+        root_node["right"], r_depth = decision_tree_learning(right_data, depth+1)
+        return (root_node, max(l_depth, r_depth))
+
+def find_split(dataset: np.ndarray) -> (dict, np.ndarray, np.ndarray):
+    node = {}
+    maxIg = 0
+    maxIgAttribute = None
+    maxIgValue = None
+    
+    for attribute in range(7):
+        (value, ig) = find_split_value(dataset, attribute)
+        if ig > maxIg:
+            maxIgValue = value
+            maxIgAttribute = attribute
+
+    node["attribute"] = maxIgAttribute
+    node["value"] = maxIgValue
+
+    return node
+
+def find_split_value(dataset: np.ndarray, attribute: int):
+        attributeValues = dataset[:, attribute]
+        sortedAttributeValues = np.sort(attributeValues)
+        for i in range(len(sortedAttributeValues)-1):
+            split_value = (sortedAttributeValues[i] + sortedAttributeValues[i+1])/2
+            
+        print(labels)
+        return(5, 2)
+
+def split_data(attribute: int, value: float) -> tuple[np.ndarray, np.ndarray]:
+    left_data = data[data[:, attribute] < value]
+    right_data = data[data[:, attribute] >= value]
+    return (left_data, right_data)
     
 if __name__ == "__main__":
     if sys.argv[1] == None:
